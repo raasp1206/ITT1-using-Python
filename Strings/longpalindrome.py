961c75947168727ca6628630a8651d72c696907a
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-07-21 15:30:56 +0530

    Create longpalindrome.py

---
+++ Strings/longpalindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        if not s or len(s) < 1:
+            return ""
+            
+        start, end = 0, 0
+        
+        for i in range(len(s)):
+            len1 = self._expand_around_center(s, i, i)
+            
+            len2 = self._expand_around_center(s, i, i + 1)
+            max_len = max(len1, len2)
+            if max_len > (end - start):
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+                
+        return s[start:end + 1]
+        
+    def _expand_around_center(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+            
+        return right - left - 1
