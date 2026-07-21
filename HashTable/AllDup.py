fd7459c75f54e1402df6d6fabbee887cb464defe
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-07-21 15:54:44 +0530

    Add Solution class to find duplicates in array

---
+++ HashTable/AllDup.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        result = []
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] < 0:
+                result.append(abs(num))
+            else:
+                nums[index] = -nums[index]      
+        return result
