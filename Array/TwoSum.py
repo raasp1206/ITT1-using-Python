cd0c3520ccff9580d241a493253b14489ca25500
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-07-23 21:30:30 +0530

    Implement TwoSum solution in Array/TwoSum.py

---
+++ Array/TwoSum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        prev_map = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prev_map:
+                return [prev_map[diff], i]
+            
+            prev_map[n] = i
+            
+        return []
