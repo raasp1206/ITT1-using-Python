a3c952abdc4a1d9216d5495dca153f4dca938d2a
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-07-23 21:42:12 +0530

    Create LargestRect.py

---
+++ Array/LargestRect.py
@@ -0,0 +1,21 @@
+class Solution:
+    def largestRectangleArea(self, heights):
+        stack = []
+        max_area = 0
+        heights.append(0)   # Sentinel to process remaining bars
+
+        for i in range(len(heights)):
+            while stack and heights[stack[-1]] > heights[i]:
+                h = heights[stack.pop()]
+
+                if stack:
+                    width = i - stack[-1] - 1
+                else:
+                    width = i
+
+                max_area = max(max_area, h * width)
+
+            stack.append(i)
+
+        heights.pop()   # Remove sentinel
+        return max_area
