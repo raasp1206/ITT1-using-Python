53a72d7f79f0b2bcb2e9cecf8b2cc8889d3294d0
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-08-11 14:46:32 +0530

    Add RemoveKDigits class with removeKdigits method
    Implement a solution to remove k digits from a number to minimize its value.

---
+++ stack/RemoveKDigits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        stack = []
+        for digit in num:
+            while k > 0 and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+        if k > 0:
+            stack = stack[:-k] 
+        result = "".join(stack).lstrip('0')
+        return result if result else "0"
