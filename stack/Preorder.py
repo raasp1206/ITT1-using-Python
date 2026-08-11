6bea513b77453eea1392ba8f752e53c93a1f1fdc
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-08-11 16:14:10 +0530

    Create Preorder.py

---
+++ stack/Preorder.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        slots = 1
+        for node in preorder.split(','):
+            if slots <= 0:
+                return False
+                
+            if node == '#':
+                slots -= 1
+            else:
+                slots += 1  
+        return slots == 0
