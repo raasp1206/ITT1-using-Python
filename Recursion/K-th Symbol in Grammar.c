6da7a91505bfa860df90ff3dc3fe9e6023ea88ac
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-09-08 16:30:47 +0530

    Create K-th Symbol in Grammar.c

---
+++ Recursion/K-th Symbol in Grammar.c
@@ -0,0 +1,14 @@
+int kthGrammar(int n, int k) {
+    if (n == 1) {
+        return 0;
+    }
+ 
+    long long mid = 1LL << (n - 2);
+    
+    if (k <= mid) {
+        return kthGrammar(n - 1, k);
+    } 
+    else {
+        return !kthGrammar(n - 1, k - mid);
+    }
+}
